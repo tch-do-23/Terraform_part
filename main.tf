a2701aa4c5992a1aa9c5e87b43cb3999fdf96834
tch-do-23 <161963143+tch-do-23@users.noreply.github.com>
2024-03-02 10:45:41 -0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,54 @@
+resource "aws_key_pair" "ssh-key" {
+  key_name   = "ssh-key"
+  public_key = file("/home/ec2-user/final_project/ssh-key.pub")
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAYS2NQTH56FFS3HC7"
+  secret_key = "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"
+
+}
+
+resource "aws_instance" "web1" {
+  ami                    = var.ami
+  instance_type          = var.instance-type[0]
+  subnet_id              = aws_subnet.tf_subnet_1.id
+  key_name               = "ssh-key"
+  vpc_security_group_ids = [aws_security_group.final_project_sg.id]
+
+
+
+  tags = {
+    Name = "Ansible"
+  }
+}
+
+resource "aws_instance" "web2" {
+  ami                    = var.ami
+  instance_type          = var.instance-type[1]
+  subnet_id              = aws_subnet.tf_subnet_1.id
+  key_name               = "ssh-key"
+  vpc_security_group_ids = [aws_security_group.final_project_sg.id]
+
+
+
+  tags = {
+    Name = " Jenkins"
+  }
+}
+
+
+
+resource "aws_instance" "web3" {
+  ami                    = var.ami
+  instance_type          = var.instance-type[2]
+  subnet_id              = aws_subnet.tf_subnet_1.id
+  key_name               = "ssh-key"
+  vpc_security_group_ids = [aws_security_group.final_project_sg.id]
+
+
+  tags = {
+    Name = "Kubernetes"
+  }
+}
